7ca83bd97c8acdf7431da2823dbd2e80c1c44955
anubd93 <anubd93@gmail.com>
2020-02-19 19:18:44 +0600

    :db config

--- config/Database.php
+++ config/Database.php
@@ -1,3 +1,28 @@
 <?php 
-	echo phpinfo();
+	class Database{
+
+		private $hostName;
+		private $userName;
+		private $password;
+		private $dbName;
+
+		private $conn;
+
+		public function connect(){
+			$this->hostName = "localhost";
+			$this->userName = "root";
+			$this->password = "";
+			$this->dbName = "rest_php_api";
+
+			$this->const  = new mysqli($this->hostName, $this->userName, $this->password, $this->dbName);
+			if ($this->conn->connect_errno) {
+				print_r($this->conn->connect_errno);
+				exit();
+			} else {
+				return $this->conn;
+			}
+
+		}
+
+	}
 ?>
