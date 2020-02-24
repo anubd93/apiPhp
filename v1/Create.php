4b60e6af9ebd752d68b1663b34a934df26d4e535
anubd93 <anubd93@gmail.com>
2020-02-24 21:25:28 +0600

    :server status maintaining

--- v1/Create.php
+++ v1/Create.php
@@ -1,4 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-type: application/json charset: UTF-8");
+header("Access-Control-Allow-Methods: POST");
+
+
 include_once ("../config/Database.php");
 include_once ("../classes/Student.php");
 
@@ -11,20 +16,45 @@ $student =  new Students($connection);
 
 
 if ($_SERVER['REQUEST_METHOD'] === "POST" ) {
-	$student->name = "ABC";
-	$student->email = "abc@xyz.com";
-	$student->mobile = "123456789";
 
-// var_dump($student); exit;
+	$data = json_decode(file_get_contents("php://input"));
 
-	if ($student->createData()) {
-		echo "Student has been created";
+	if(!empty($data->name) && !empty($data->email) && !empty($data->mobile)){
+			// print_r($data);die();
+
+			$student->name = $data->name;
+			$student->email = $data->email;
+			$student->mobile = $data->mobile;
+
+		// var_dump($student); exit;
+
+			if ($student->createData()) {
+				http_response_code(200);
+				echo json_encode(array(
+            		"status" => 1,
+            		"message" => "Student has been created"
+				));
+			} else {
+				http_response_code(500);
+				echo json_encode(array(
+            		"status" => 0,
+            		"message" => "Failed to insert data"
+				));
+			}
 	} else {
-		echo "Failed to insert data";
+		http_response_code(404);
+		echo json_encode(array(
+         	"status" => 0,
+            "message" => "All values needed"
+		));
 	}
 
 } else {
-	echo "Access denied";
+	http_response_code(503);
+	echo json_encode(array(
+		"status" => 0,
+		"message" => "Access denied"
+	));
 }
 
 ?>
